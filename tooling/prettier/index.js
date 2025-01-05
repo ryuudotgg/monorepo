146660ed235d06c9d1ab15c3f550a668f9fea7fb
Nemanja Stanić <hai@purr.gg>
2025-01-05 20:36:07 +0100

    feat(tooling): add a prettier config

---
+++ tooling/prettier/index.js
@@ -0,0 +1,34 @@
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@purr",
+    "^@purr/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    { files: "*.json.hbs", options: { parser: "json" } },
+    { files: "*.js.hbs", options: { parser: "babel" } },
+    { files: "*.ts.hbs", options: { parser: "babel" } },
+  ],
+};
+
+export default config;
