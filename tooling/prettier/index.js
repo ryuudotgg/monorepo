06e4f148f777f3bb6f75d7a48a802f16319061b4
Nemanja Stanić <hai@purr.gg>
2025-01-05 20:40:27 +0100

    fix(repo): add format scripts to root

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -1,12 +1,9 @@
-
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
 /** @type { PrettierConfig | SortImportsConfig } */
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-  ],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
   importOrder: [
     "<TYPES>",
     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
