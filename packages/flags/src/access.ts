a7aa201d8f8d3da7435ec14d0c3c25c03b2fd366
Ryuu <me@ryuu.gg>
2025-01-10 02:23:47 +0100

    feat(flags): feature flags with @vercel/flags and launchdarkly (#53)

---
+++ packages/flags/src/access.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from "next/server";
+import { verifyAccess } from "@vercel/flags";
+
+import * as flags from ".";
+
+async function GET(request: NextRequest): Promise<Response> {
+  const access = await verifyAccess(request.headers.get("Authorization"));
+  if (!access)
+    return new Response(
+      JSON.stringify({ code: "UNAUTHORIZED", message: "Unauthorized" }),
+      { status: 401, headers: { "Content-Type": "application/json" } },
+    );
+
+  const definitions = Object.fromEntries(
+    Object.entries(flags).map(([key, flag]) => [
+      key,
+      {
+        origin: flag.origin,
+        description: flag.description,
+        options: flag.options,
+      },
+    ]),
+  );
+
+  return new Response(JSON.stringify({ definitions }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export { GET };
