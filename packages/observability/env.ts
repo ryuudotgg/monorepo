9008a2ce22e7f11323b57e19f13358eb6e3daaab
nemanja <hai@purr.gg>
2025-01-10 02:34:29 +0100

    feat(observability): add loggers, and uptime status helper (#54)

---
+++ packages/observability/env.ts
@@ -0,0 +1,33 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+import shared from "@purr/env";
+
+export const env = createEnv({
+  extends: [shared],
+
+  shared: {},
+
+  server: {
+    BETTERSTACK_UPTIME_API_KEY: z.string().trim().min(1),
+
+    SENTRY_ORG: z.string().trim().min(1),
+    SENTRY_PROJECT: z.string().trim().min(1),
+  },
+
+  client: {
+    NEXT_PUBLIC_SENTRY_DSN: z.string().url(),
+  },
+
+  runtimeEnv: {
+    BETTERSTACK_UPTIME_API_KEY: process.env.BETTERSTACK_UPTIME_API_KEY,
+
+    SENTRY_ORG: process.env.SENTRY_ORG,
+    SENTRY_PROJECT: process.env.SENTRY_PROJECT,
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  },
+
+  emptyStringAsUndefined: true,
+  skipValidation:
+    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+});
---
+++ packages/observability/src/error.ts
@@ -0,0 +1,49 @@
+import type { Geo } from "@vercel/functions";
+import { captureException } from "@sentry/nextjs";
+
+import type { Session } from "@purr/auth";
+
+import { log } from "./log";
+
+function processError(
+  error: unknown,
+  session?: { user?: Session["user"]; ip?: string; geo?: Geo },
+  extra?: Record<string, unknown>,
+): string {
+  let message = "An error occurred";
+
+  if (error instanceof Error) message = error.message;
+  else if (error && typeof error === "object" && "message" in error)
+    message = error.message as string;
+  else message = String(error);
+
+  try {
+    captureException(error, {
+      user: session && {
+        id: session.user?.id,
+        email: session.user?.email,
+        username: session.user?.username,
+        ip_address: session.ip,
+        geo: session.geo && {
+          city: session.geo.city,
+          region: session.geo.countryRegion,
+          country_code: session.geo.country,
+        },
+      },
+      extra,
+    });
+
+    log.error(message, { error });
+  } catch (newError) {
+    log.error("Process Error Failed", {
+      newError,
+
+      originalError: error,
+      originalMessage: message,
+    });
+  }
+
+  return message;
+}
+
+export { processError };
