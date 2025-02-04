856b41b3e1fe7ef68c67f5223bd18a55aa4a859c
Ryuu <me@ryuu.gg>
2025-02-04 19:28:59 +0100

    fix(ci): remove unnecessary non-null assertion

--- packages/analytics/src/posthog/client.tsx
+++ packages/analytics/src/posthog/client.tsx
@@ -6,14 +6,13 @@ import { PostHogProvider as PostHogProviderRaw } from "posthog-js/react";
 
 import { env } from "../../env";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const analytics = posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
   ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
   api_host: "/ingest",
   capture_pageleave: true,
   capture_pageview: false,
   person_profiles: "identified_only",
-})!;
+});
 
 function PostHogProvider({ children }: { children: ReactNode }) {
   return <PostHogProviderRaw client={analytics}>{children}</PostHogProviderRaw>;
