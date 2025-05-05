ddaf6a492f809218ac720f812fd2bd68b58c81e7
Ryuu <me@ryuu.gg>
2025-05-05 11:27:44 +0400

    feat(cache): replace vercel kv with upstash directly (#683)

--- packages/cache/env.ts
+++ packages/cache/env.ts
@@ -9,17 +9,17 @@ export const env = createEnv({
   shared: {},
 
   server: {
-    KV_URL: z.string().url(),
-    KV_REST_API_URL: z.string().url(),
-    KV_REST_API_TOKEN: z.string().trim().min(1),
+    UPSTASH_REDIS_URL: z.string().url(),
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string().trim().min(1),
   },
 
   client: {},
 
   runtimeEnv: {
-    KV_URL: process.env.KV_URL,
-    KV_REST_API_URL: process.env.KV_REST_API_URL,
-    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    UPSTASH_REDIS_URL: process.env.UPSTASH_REDIS_URL,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
 
   emptyStringAsUndefined: true,
--- packages/cache/src/index.ts
+++ packages/cache/src/index.ts
@@ -1,8 +1,10 @@
-import { createClient } from "@vercel/kv";
+import { Redis } from "@upstash/redis/cloudflare";
 
 import { env } from "../env";
 
-export const cache = createClient({
-  url: env.KV_REST_API_URL,
-  token: env.KV_REST_API_TOKEN,
+export const cache = new Redis({
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_TOKEN,
+  enableAutoPipelining: true,
+  enableTelemetry: false,
 });
